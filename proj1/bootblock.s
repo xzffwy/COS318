fed2a44a534fc6d4bb5fda339542437459201e53
Robert A. Sami <rsami@fc015-labpc-04.cs.princeton.edu>
2013-09-27 17:41:39 -0400

    first iteration full bootblock

--- proj1/bootblock.s
+++ proj1/bootblock.s
@@ -34,21 +34,28 @@ os_size:
 # setup registers for kernel data and disk read	
 load_kernel:  	
 	#set bootloader stack segment
-	movw $0x07c0, %ax
+	movw $0x0000, %ax
 	movw %ax, %ss
+	#set extra segment to 0
+	movw %ax, %es
 	#set bootloader base and stack pointer
-	movw $0x10, %ax
-	movw %ax, %bp
-	movw %ax, %sp
+	mov $0xA0000, %eax
+	mov %eax, %ebp
+	mov %eax, %esp
 
 	#load kernel data
 	movb $0x02, %ah
-	movb $0x01, %al #sectors to read
+	movb os_size, %al #sectors to read = 128
+	movw $0x0001, %cx #cylinder = 0, sector = 1
+	movb $0x00, %dh #head = 0, dl should be as it was set by BIOS
 
+	movw $0x1000, %bx #set write destination of kernel
 
+	enter $0, $0
 
+	int $0x13
 
-
+	leave
 
 
 # setup the kernel stack
@@ -64,20 +71,24 @@ setup_stack:
 # switch control to kernel
 switch_to_kernel:
 	#set kernel data segment
-	movw $0x07c0, %ax
+	movw $BOOT_SEGMENT, %ax
 	movw %ax, %ds
 
-	
+	#jump to kernel
+	ljmp 0x1000
+
 
 # print a character to screen at the position of the cursor. TODO: advance the cursor
 print_char:
-	enter $0,$0
-
+	push %ebx
+	
 	mov $0x0e,%ah #specify teletype
 	mov $0x00,%bh #page number
 	mov $0x02,%bl #color of foreground (white)
 
+	push %edx
 	int $0x10 #call interrupt
+	pop %edx
 		
-	leave
+	pop %ebx
 	ret
