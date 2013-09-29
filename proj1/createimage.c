78187fa345f5f5f0f2dfa046e01c0abae18b69d9
John P. McSpedon <mcspedon@fc010-labpc-15.cs.princeton.edu>
2013-09-29 17:58:30 -0400

    basic version of createimage

--- proj1/createimage.c
+++ proj1/createimage.c
@@ -21,29 +21,25 @@
 /* Reads in an executable file in ELF format*/
 Elf32_Phdr * read_exec_file(FILE **execfile, char *filename, Elf32_Ehdr **ehdr){
   size_t numread;
-  Elf32_Phdr *phdr = malloc(sizeof(Elf32_Phdr)); //ELF Program header TODO:wrap in error check
+  Elf32_Phdr *phdr = malloc(sizeof(Elf32_Phdr)); //ELF Program header
+  // TODO: error stuff
   
-  // read sizeof(Elf32_Ehdr) bytes into buffer ehdr
+  // read in ELF header i.e. read sizeof(Elf32_Ehdr) bytes into buffer ehdr
   numread = fread(*ehdr, 1, sizeof(Elf32_Ehdr), *execfile);  
   //TODO: error stuff
 
-  
-  // populate ehdr
-  ehdr
-
- 
-  // read in elf header CHECK
-  numread = fread(&ehdr, sizeof(Elf32_Ehdr), &execfile)
+  //Debug
+  printf("I think e_phentsize is %d", (**ehdr).e_phentsize);
   
   // populate fields
-  phdr.p_type = ;
-  phdr.p_offset = ;
-  phdr.p_vaddr = ;
-  phdr.p_paddr = ;
-  phdr.p_filesz = ;
-  phdr.p_memsz = ;
-  phdr.p_flags = ;
-  phdr.p_align = ;
+  (*phdr).p_type = NULL;
+  (*phdr).p_offset = NULL;
+  (*phdr).p_vaddr = NULL;
+  (*phdr).p_paddr = NULL;
+  (*phdr).p_filesz = NULL;
+  (*phdr).p_memsz = NULL;
+  (*phdr).p_flags = NULL;
+  (*phdr).p_align = NULL;
 
   return NULL;
 }
@@ -63,7 +59,7 @@ void write_kernel(FILE **imagefile, FILE *kernelfile, Elf32_Ehdr *kernel_header,
 /* Counts the number of sectors in the kernel */
 int count_kernel_sectors(Elf32_Ehdr *kernel_header, Elf32_Phdr *kernel_phdr){
     
-    return 0;
+  return 0;
 }
 
 /* Records the number of sectors in the kernel */
@@ -102,32 +98,32 @@ int main(int argc, char **argv){
   Elf32_Ehdr *boot_header = malloc(sizeof(Elf32_Ehdr));//bootblock ELF header
   Elf32_Ehdr *kernel_header = malloc(sizeof(Elf32_Ehdr));//kernel ELF header
 
-  Elf32_Phdr *boot_program_header; //bootblock ELF program header
-  Elf32_Phdr *kernel_program_header; //kernel ELF program header
+  Elf32_Phdr *boot_phdr; //bootblock ELF program header
+  Elf32_Phdr *kernel_phdr; //kernel ELF program header
 
   /* check for legal num of args */
   if (argc != 3 && argc !=4) {
 	fprintf(stderr, "Error: call as '%s (--extended) bootblock-location kernel-location'\n", argv[0]);
-        exit(EXIT_FAILURE);
+    exit(EXIT_FAILURE);
   }
 
-  /* create image file */
-  if(!(imagefile = fopen(IMAGE_FILE, 'w+')i)) {
-      fprintf(stderr, "Error creating %s: %s\n", IMAGE_FILE, strerror(errno));
-        exit(EXIT_FAILURE);
+  /* open image file for writing*/
+  if(!(imagefile = fopen(IMAGE_FILE, "w+"))) {
+    fprintf(stderr, "Error creating %s: %s\n", IMAGE_FILE, strerror(errno));
+    exit(EXIT_FAILURE);
   }
 
 
   /* read executable bootblock file   
    *     note that here filename = argv[argc-2]; */
-  if(!(bootfile = fopen(argv[argc-2], 'r'))){
-        fprintf(stderr, "Error reading %s: %s\n", argv[argc-2], strerror(errno));
-        exit(EXIT_FAILURE);
+  if(!(bootfile = fopen(argv[argc-2], "r"))){
+    fprintf(stderr, "Error reading %s: %s\n", argv[argc-2], strerror(errno));
+    exit(EXIT_FAILURE);
   }
-  boot_phdr = read_exec_file(&execfile, argv[argc-2], &boot_header);
+  boot_phdr = read_exec_file(&bootfile, argv[argc-2], &boot_header);
 
   /* write bootblock */  
-  write_bootblock(&imagefile, bootfile, boot_header, boot_phdr)
+  write_bootblock(&imagefile, bootfile, boot_header, boot_phdr);
 
   /* read executable kernel file */
 
