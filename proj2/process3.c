9036ccec888e962e3c0564bdfd81e2e3457c6453
John P. McSpedon <mcspedon@fc010-labpc-02.cs.princeton.edu>
2013-10-13 18:44:37 -0400

    process timing

--- proj2/process3.c
+++ proj2/process3.c
@@ -5,14 +5,25 @@
 #define MAXROW 25
 
 char * display_str = "Cycles required per yield(): ";
+extern uint64_t recorded_time = 18446744073709551615; //maxval of 64-bit int
 
 void _start(void)
 {
-	uint64_t recorded_time;
-	recorded_time = get_timer();
-	yield();
-	recorded_time = get_timer() - recorded_time;
-	print_str(MAXROW-1, 0, display_str);
-	print_int(MAXROW-1, strlen(display_str), (int) recorded_time);
-    exit();
+	uint64_t time;
+	/* grab time immediately so other calculations don't come into play */
+	time = get_timer();
+	
+	/* if this is the first process to come about, record time, yield, and exit */
+	if (time < recorded_time) {
+		recorded_time = get_timer();
+		yield();
+		exit();
+	}
+	/* second process calculates difference in time then exits */
+	else {
+		print_str(MAXROW-7, 0, display_str);
+		print_int(MAXROW-7, strlen(display_str), (int) (time - recorded_time));
+    	exit();
+	}
+	
 }
