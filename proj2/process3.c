f029d42c37a12dc98697f4c4c854673a54ebb230
John P. McSpedon <mcspedon@fc010-labpc-02.cs.princeton.edu>
2013-10-13 15:48:47 -0400

    basic timing code

--- proj2/process3.c
+++ proj2/process3.c
@@ -2,8 +2,17 @@
 #include "syslib.h"
 #include "util.h"
 
+#define MAXROW 25
+
+char * display_str = "Cycles required per yield(): ";
 
 void _start(void)
 {
+	uint64_t recorded_time;
+	recorded_time = get_timer();
+	yield();
+	recorded_time = get_timer() - recorded_time;
+	print_str(MAXROW-1, 0, display_str);
+	print_int(MAXROW-1, strlen(display_str), (int) recorded_time);
     exit();
 }
--- proj2/th3.c
+++ proj2/th3.c
@@ -2,11 +2,30 @@
 #include "scheduler.h"
 #include "util.h"
 
+#define MAXROW 25
+
+uint64_t recorded_time;
+char * display_str = "Cycles required per do_yield(): ";
+
+
+// Thread 4 active
+//     record a time; yield immediately
+// Thread 5 is next on queue (queue initialized round-robin), becomes active
+//     record a time; display difference in times
 
 void thread4(void)
 {
+	recorded_time = get_timer();
+	do_yield();
+	do_exit();
 }
 
 void thread5(void)
 {
+	recorded_time = get_timer() - recorded_time;
+
+	print_str(MAXROW-2, 0, display_str);
+	print_int(MAXROW-2, strlen(display_str), (int) recorded_time);
+
+	do_exit();
 }
