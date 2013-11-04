abb25be021479a201ad8283eab10c537b7fb9108
John McSpedon <johnmc1547@gmail.com>
2013-11-04 00:13:38 -0500

    changes during submission in lab
    uncommented assembly. changed order of check_sleeping in scheduler.c

--- proj3/scheduler.c
+++ proj3/scheduler.c
@@ -17,7 +17,23 @@ node_t sleep_wait_queue;
 // more variables...
 volatile uint64_t time_elapsed;
 
-/* TODO: Round-robin scheduling: Save current_running before preempting */
+/* wake up sleeping processes whose deadlines have passed */
+void check_sleeping(){
+  pcb_t *sleeping;
+
+  while (!is_empty(&sleep_wait_queue)) {
+    sleeping = (pcb_t *)peek(&sleep_wait_queue);
+    if (sleeping->deadline <= time_elapsed) {
+      sleeping->status = READY;
+      enqueue(&ready_queue, dequeue(&sleep_wait_queue));
+    } else {
+      return;
+    }
+  }
+
+}
+
+/* Round-robin scheduling: Save current_running before preempting */
 void put_current_running(){
   // TO-DO: Do we want to wake up sleeping processes here as well?
   check_sleeping();
@@ -66,23 +82,6 @@ void do_sleep(int milliseconds){
 
   scheduler_entry();
   leave_critical(); 
-}
-
-/* TODO: Check if we can wake up sleeping processes */
-void check_sleeping(){
-  pcb_t *sleeping;
-
-  while (!is_empty(&sleep_wait_queue)) {
-    sleeping = (pcb_t *)peek(&sleep_wait_queue);
-    if (sleeping->deadline <= time_elapsed) {
-      sleeping->status = READY;
-      enqueue(&ready_queue, dequeue(&sleep_wait_queue));
-      //printf(12,0,"moved process with pid %d from sleeping to ready", sleeping->pid); //DEBUG
-    } else {
-      return;
-    }
-  }
-
 }
 
 /* Do not modify any of the following functions */
